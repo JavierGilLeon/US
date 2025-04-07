34daeb284529eb5db552f4e889d32a2396608fbd
Javier Gil <javiergilleon14@gmail.com>
2025-04-07 17:40:50 +0200

    Cambios

--- Fundamentos_Robotica/Ejercicios/Ejercicio1.m
+++ Fundamentos_Robotica/Ejercicios/Ejercicio1.m
@@ -1,11 +1,14 @@
-
+clc
+clear
 syms L0 L1 L2 L3A L3B real
 syms q1 q2 q3 real;
 
 PI = sym(pi);
-TB0 = MDH(L0,0,0,0);
-A01 = MDH(0,PI/2+q1,L1,PI/2);
-A12 = MDH(L2,q2,0,PI/2);
-A23 = MDH(L1A,PI/2,L3B,0);
+TB0 = MDH(0,L0,0,0);
+A01 = MDH(PI/2+q1,0,L1,PI/2);
+A12 = MDH(q2,L2,0,PI/2);
+A23 = MDH(PI/2,L3A+q3,L3B,0);
 
-TB3 = simplify(TB0*A01*A12*A23)
+TB3 = simplify(TB0*A01*A12*A23);
+
+subs(TB3,[q1,q2,q3],[0,0,0])
